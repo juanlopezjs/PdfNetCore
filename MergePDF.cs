ade9e359ad0f46eaa17468317a57f6f1628ea102
juanma0474 <juanma0474@hotmail.com>
2020-03-15 12:04:58 -0500

    commit inicial

---
+++ MergePDF.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+namespace MergePDF
+{
+    public class PDF
+    {
+        public void MergePDF(string[] fileNames, string outFile)
+        {
+            Document document = new Document();
+            //create newFileStream object which will be disposed at the end
+            FileStream newFileStream = new FileStream(outFile, FileMode.Create);
+            // step 2: we create a writer that listens to the document
+            PdfCopy writer = new PdfCopy(document, newFileStream);
+
+            // step 3: we open the document
+            document.Open();
+
+            foreach (string fileName in fileNames)
+            {
+                // we create a reader for a certain document
+                PdfReader reader = new PdfReader(fileName);
+                reader.ConsolidateNamedDestinations();
+
+                // step 4: we add content
+                for (int i = 1; i <= reader.NumberOfPages; i++)
+                {
+                    writer.AddPage(writer.GetImportedPage(reader, i));
+                }
+
+                reader.Close();
+            }
+
+            // step 5: we close the document and writer
+            writer.Close();
+            document.Close();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MergePDF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] filesNames = {
+                "/root/file1.pdf",
+                "/root/file2.pdf"
+            };
+
+            string outFile = "/finalRoot/file.pdf";
+
+            var pdf = new PDF();
+            pdf.MergePDF(filesNames, outFile);
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
