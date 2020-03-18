c174634afe5fd4179cd735fc9635bf7cb0b1dab7
juanma0474 <juanma0474@hotmail.com>
2020-03-17 23:37:40 -0500

    Se agrega metodo para modificar un pdf y agregar una imagen como firma digital

--- MergePDF.cs
+++ MergePDF.cs
@@ -1,4 +1,7 @@
-﻿using iText.Kernel.Pdf;
+﻿using iText.IO.Image;
+using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
 
 namespace MergePDF
 {
@@ -19,6 +22,28 @@ namespace MergePDF
             }
 
             pdfDoc.Close();
+            
+        }
+
+        public void AddImageToPDF(string pdfFileName, string imageFileName, string outFile)
+        {
+            // Modify PDF located at "pdfFileName" and save to "outFile"
+            PdfDocument pdfDocument = new PdfDocument(new PdfReader(pdfFileName), new PdfWriter(outFile));
+            // Document to add layout elements: paragraphs, images etc
+            Document document = new Document(pdfDocument);
+
+            // Load image from disk
+            ImageData imageData = ImageDataFactory.Create(imageFileName);
+            // Create layout image object and provide parameters. Page number = 1
+            Image image = new Image(imageData)
+                .ScaleAbsolute(100, 100)
+                .SetFixedPosition(pdfDocument.GetNumberOfPages(), 25, 25);
+            // This adds the image to the page
+            document.Add(image);
+
+            // Don't forget to close the document.
+            // When you use Document, you should close it rather than PdfDocument instance
+            document.Close();
         }
     }
 }
