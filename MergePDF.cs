7f4c7d15278e03bde9a76c06da972113644689bc
juanma0474 <juanma0474@hotmail.com>
2020-03-17 23:48:14 -0500

    Se organiza el código

--- MergePDF.cs
+++ MergePDF.cs
@@ -9,33 +9,33 @@ namespace MergePDF
     {
         public void MergePDF(string[] fileNames, string outFile)
         {
-            var writer = new PdfWriter(outFile);
-            var pdfDoc = new PdfDocument(writer);
+            var pdfWriter = new PdfWriter(outFile);
+            var finalPdfDocument = new PdfDocument(pdfWriter);
 
-            pdfDoc.InitializeOutlines();
+            finalPdfDocument.InitializeOutlines();
 
             foreach (string fileName in fileNames)
             {
-                var addedDoc = new PdfDocument(new PdfReader(fileName));
-                addedDoc.CopyPagesTo(1, addedDoc.GetNumberOfPages(), pdfDoc);
-                addedDoc.Close();
+                var pdfDocument = new PdfDocument(new PdfReader(fileName));
+                pdfDocument.CopyPagesTo(1, pdfDocument.GetNumberOfPages(), finalPdfDocument);
+                pdfDocument.Close();
             }
 
-            pdfDoc.Close();
+            finalPdfDocument.Close();
             
         }
 
         public void AddImageToPDF(string pdfFileName, string imageFileName, string outFile)
         {
             // Modify PDF located at "pdfFileName" and save to "outFile"
-            PdfDocument pdfDocument = new PdfDocument(new PdfReader(pdfFileName), new PdfWriter(outFile));
+            var pdfDocument = new PdfDocument(new PdfReader(pdfFileName), new PdfWriter(outFile));
             // Document to add layout elements: paragraphs, images etc
-            Document document = new Document(pdfDocument);
+            var document = new Document(pdfDocument);
 
             // Load image from disk
-            ImageData imageData = ImageDataFactory.Create(imageFileName);
+            var imageData = ImageDataFactory.Create(imageFileName);
             // Create layout image object and provide parameters. Page number = 1
-            Image image = new Image(imageData)
+            var image = new Image(imageData)
                 .ScaleAbsolute(100, 100)
                 .SetFixedPosition(pdfDocument.GetNumberOfPages(), 25, 25);
             // This adds the image to the page
